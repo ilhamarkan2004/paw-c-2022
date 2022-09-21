8d809abdb1182cd5b7784c5cf3826e4f434f306a
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2022-09-21 23:00:46 +0700

    Update konversi.php

--- PAW-C-2022/public/konversi.php
+++ PAW-C-2022/public/konversi.php
@@ -90,3 +90,45 @@
                 ><span class="lg:hidden inline-block ml-2">Star</span></a
               >
             </li>
+
+            <li class="flex items-center">
+              <a href="register.php">
+              <button
+                class="bg-white text-gray-800 active:bg-gray-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+                type="button"
+                style="transition: all 0.15s ease 0s;"
+              >
+                <i class="fas fa-arrow-alt-circle-down"></i> Registrasi
+              </button></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <main>
+      <section class="absolute w-full h-full">
+        <div
+          class="absolute top-0 w-full h-full bg-gray-900"
+          style="background-image: url(img/assets/img/register_bg_2.png); background-size: 100%; background-repeat: no-repeat;"
+        ></div>
+        <div class="container mx-auto px-4 h-full">
+          <div class="flex content-center items-center justify-center h-full">
+            <div class="w-full lg:w-4/12 px-4">
+              <div
+                class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0"
+              >
+                <div class="rounded-t mb-0 px-6 py-6" data-aos="zoom-out">
+                  <div class="text-center mb-3">
+                    <h6 class="text-gray-600 text-sm font-bold" data-aos="zoom-out">
+                      PERHITUNGAN NILAI KONVERSI
+                    </h6>
+                  </div>
+                  <div class="btn-wrapper text-center">
+                    
+                  </div>
+                  <hr class="mt-6 border-b-1 border-gray-400" />
+                </div>
+                <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+                  <div class="text-gray-500 text-center mb-3 font-bold" data-aos="zoom-out">
+                    <small>Masukkan nilai IPK anda!</small>
+                  </div>
