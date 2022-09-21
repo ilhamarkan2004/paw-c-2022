4cf4aeda54e5c51dd055b77702367d830dae9331
danialrasyid <danialrasyid1@student.ub.ac.id>
2022-09-21 22:56:32 +0700

    Update register.php

--- PAW-C-2022/register.php
+++ PAW-C-2022/register.php
@@ -106,3 +106,119 @@
       </div>
     </nav>
     <main>
+    <section class="absolute w-full h-full">
+        <div
+          class="absolute top-0 w-full h-full bg-gray-900"
+          style="background-image: url(img/assets/img/register_bg_2.png); background-size: 100%; background-repeat: no-repeat;"
+        ></div>
+        <div class="container mx-auto px-4 h-full">
+          <div class="flex content-center items-center justify-center h-full">
+            <div class="w-full lg:w-4/12 px-4">
+              <div
+                class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0"
+              >
+                <div class="rounded-t mb-0 px-6 py-6" data-aos="zoom-out">
+                  <div class="text-center mb-3">
+                    <h6 class="text-gray-600 text-sm font-bold" data-aos="zoom-out">
+                      Sign in with
+                    </h6>
+                  </div>
+                  <div class="btn-wrapper text-center">
+                    <button data-aos="zoom-out"
+                      class="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
+                      type="button"
+                      style="transition: all 0.15s ease 0s;"
+                    >
+                      <img data-aos="zoom-out"
+                        alt="..."
+                        class="w-5 mr-1"
+                        src="img/assets/img/github.svg"
+                      />Github</button
+                    ><button
+                    data-aos="zoom-out"
+                      class="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
+                      type="button"
+                      style="transition: all 0.15s ease 0s;"
+                    >
+                      <img
+                        alt="..."
+                        class="w-5 mr-1"
+                        src="img/assets/img/google.svg"
+                      />Google
+                    </button>
+                  </div>
+                  <hr class="mt-6 border-b-1 border-gray-400" />
+                </div>
+                <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+                  <div class="text-gray-500 text-center mb-3 font-bold" data-aos="zoom-out">
+                    <small>Or sign in with credentials</small>
+                  </div>
+                  <form action="" method="post" id="submit">
+                    <div class="relative w-full mb-3">
+                      <label
+                      data-aos="zoom-out"
+                        class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        for="email"
+                        >Email</label
+                      ><input
+                        type="email"
+                        data-aos="zoom-out"
+                        class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                        placeholder="Email"
+                        style="transition: all 0.15s ease 0s;" id="email" name=""email
+                      />
+                    </div>
+                    <div class="relative w-full mb-3">
+                        <label
+                        data-aos="zoom-out"
+                          class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                          for="username"
+                          >Username</label
+                        ><input
+                        data-aos="zoom-out"
+                          type="text"
+                          class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                          placeholder="Username"
+                          style="transition: all 0.15s ease 0s;" id="username" name = "username"
+                        />
+                      </div>
+                    <div class="relative w-full mb-3">
+                      <label
+                      data-aos="zoom-out"
+                        class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        for="password"
+                        >Password</label
+                      ><input
+                      data-aos="zoom-out"
+                        type="password"
+                        class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                        placeholder="Password"
+                        style="transition: all 0.15s ease 0s;" id="password" name ="password"
+                      />
+                    </div>
+                    <div>
+                      <label class="inline-flex items-center cursor-pointer" data-aos="zoom-out"
+                        ><input
+                        data-aos="zoom-out"
+                          id="customCheckLogin"
+                          type="checkbox"
+                          class="form-checkbox border-0 rounded text-gray-800 ml-1 w-5 h-5"
+                          style="transition: all 0.15s ease 0s;"
+                        /><span class="ml-2 text-sm font-semibold text-gray-700"
+                          >Remember me</span
+                        ></label
+                      >
+                    </div>
+                    <div class="text-center mt-6">
+                      <button
+                      data-aos="zoom-out"
+                        class="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+                        type="submit"
+                        style="transition: all 0.15s ease 0s;" name ="register" onclick="showAlert()"
+                      >
+                        Registrasi
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
