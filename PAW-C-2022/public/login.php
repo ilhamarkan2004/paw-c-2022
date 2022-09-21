579a598e54482ad01a431c70cc156ba66ff08e7c
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2022-09-21 22:54:51 +0700

    Update login.php

--- PAW-C-2022/public/login.php
+++ PAW-C-2022/public/login.php
@@ -224,3 +224,39 @@ function display_login_form(){ ?>
                       >
                         Log In
                       </button>
+
+                      </div>
+                  </form>
+                </div>
+              </div>
+              <div class="flex flex-wrap mt-6">
+                <div class="w-1/2">
+                  <a href="" class="text-gray-300"
+                    ><small>Forgot password?</small></a
+                  >
+                </div>
+                <div class="w-1/2 text-right">
+                  <a href="" class="text-gray-300"
+                    ><small>Create new account</small></a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer class="absolute w-full bottom-0 bg-gray-900 pb-6">
+          <div class="container mx-auto px-4">
+            <hr class="mb-6 border-b-1 border-gray-700" />
+            <div
+              class="flex flex-wrap items-center md:justify-between justify-center"
+            >
+              <div class="w-full md:w-4/12 px-4">
+                <div class="text-sm text-white font-semibold py-1">
+                  Copyright © 2021
+                  <a
+                    href=""
+                    class="text-white hover:text-gray-400 text-sm font-semibold py-1"
+                    >Kelompok 3 PAW</a
+                  >
+                </div>
+              </div>
